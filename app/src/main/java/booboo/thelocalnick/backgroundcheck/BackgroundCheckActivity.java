c91e12bf52d3ded7aee7d1b86776f53865e2c9ce
ishankothari <ishankothari@gmail.com>
2017-02-17 17:43:04 -0800

    Added background verification page & added logo to sign in and create account pages.

---
+++ app/src/main/java/booboo/thelocalnick/backgroundcheck/BackgroundCheckActivity.java
@@ -0,0 +1,26 @@
+package booboo.thelocalnick.backgroundcheck;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import booboo.thelocalnick.R;
+
+/**
+ * Created by ishankothari on 2/16/17.
+ */
+
+public class BackgroundCheckActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backgroud_check);
+
+        Spinner spinnerStates = (Spinner) findViewById(R.id.spinnerStates);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.us_states, android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerStates.setAdapter(arrayAdapter);
+    }
+}
--- app/src/main/java/booboo/thelocalnick/createaccount/CreateAccountFragment.java
+++ app/src/main/java/booboo/thelocalnick/createaccount/CreateAccountFragment.java
@@ -44,32 +44,7 @@ public class CreateAccountFragment extends Fragment {
                             }
                         }).create();
                 dialog.setTitle("Terms of Use");
-                dialog.setMessage("Lorem ipsum dolor sit amet, dolorum minimum definitionem cum " +
-                        "ei, cum ei postea detraxit ullamcorper. Justo homero everti ad eam, an" +
-                        " vim nulla choro. Sea ne novum detraxit, idque nostrud utroque has id. " +
-                        "Illum noster similique eos ad, ut tritani theophrastus interpretaris sed." +
-                        " Dicant utroque euripidis ut mei, has ex bonorum lucilius disputando.\n" +
-                        "\n" +
-                        "Persius sapientem pro ne, no nec ludus adipisci vituperata. Cum ut exerci" +
-                        " sadipscing. Has qualisque philosophia at. Pro nihil nemore adipiscing id," +
-                        " at integre legendos vix. Eam ad eros alienum.\n" +
-                        "\n" +
-                        "Ut mel partem detraxit, eam no senserit argumentum. Tractatos mediocritatem" +
-                        " ei mei, ea per ceteros rationibus. Et tation virtute assentior his, " +
-                        "utroque suscipit salutatus cum et, pro et illud sonet inimicus. His homero" +
-                        " conceptam in, vim id idque quando ullamcorper, iudico vocibus at cum." +
-                        " Causae deleniti efficiendi at est.\n" +
-                        "\n" +
-                        "Iudico populo partiendo quo an. Nusquam detracto legendos vis ne, his id" +
-                        " elit accumsan copiosae. Noster scaevola honestatis in pri, eum at graece" +
-                        " prodesset. Numquam senserit volutpat no qui, sea porro oporteat conceptam" +
-                        " ei. Ad mea veri volumus phaedrum, eius civibus voluptaria et mel. Eu nec" +
-                        " putent alienum, cibo euripidis voluptatum no eos.\n" +
-                        "\n" +
-                        "Id vel nusquam delicatissimi, qui solet antiopam persecuti ut, doming" +
-                        " commune voluptaria eum in. Vel ea primis persecuti. Ius erat eligendi ex." +
-                        " An has commodo incorrupte.");
-
+                dialog.setMessage(getString(R.string.terms_of_use));
                 dialog.show();
             }
         });
