f61335dc1e67284cf1206350d897df7c4c5fcc62
ashwinkumar91 <araghave@usc.edu>
2017-04-26 20:41:45 -0700

    Janhavi (#25)
    * fixed the geolocation problem
    
    * Stopped location tracking

--- app/src/main/java/booboo/thelocalnick/tourListing/HomeScreenActivity.kt
+++ app/src/main/java/booboo/thelocalnick/tourListing/HomeScreenActivity.kt
@@ -403,6 +403,8 @@ override fun onCreateOptionsMenu(menu: Menu): Boolean {
                     }
                     val textLocation = findViewById(R.id.location) as TextView
                     textLocation.text = cityName
+                    locationManager.removeUpdates(this)
+
                     //do something with Lat and Lng
                 }
 
